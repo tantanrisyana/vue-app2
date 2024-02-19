a10fbe766d14cf72bde0315fca0b99da4b765728
unknown <jto@ITLTDELW101921.JTO.co.id>
2024-02-19 09:48:25 +0700

    Devextreme vue json data

---
+++ other vue/json-data/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <DxDataGrid
+  :columns="columns"
+  :show-borders="true"
+  :data-source="dataSource"
+/>
+</template>
+
+<script setup lang="ts">
+import DxDataGrid from 'devextreme-vue/data-grid';
+
+const columns = ['CompanyName', 'City', 'State', 'Phone', 'Fax'];
+const dataSource = '/data/customers.json';
+</script>
+
