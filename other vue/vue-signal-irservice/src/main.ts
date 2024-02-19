5ab0a53b559e323ad2efb51df423a452ea1ad3c3
unknown <jto@ITLTDELW101921.JTO.co.id>
2024-02-19 11:35:14 +0700

    Devextreme vue SignalIR Service

---
+++ other vue/vue-signal-irservice/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import 'devextreme/dist/css/dx.common.css'
+import 'devextreme/dist/css/dx.material.blue.light.compact.css'
+
+createApp(App).mount('#app')
